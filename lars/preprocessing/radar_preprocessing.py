77073d58c4b3200147a20bedba67aaea03f09a0d
Bobby Jackson <rjackson@anl.gov>
2026-01-15 10:07:11 -0500

    ADD: Option for using a different tilt.

--- lars/preprocessing/radar_preprocessing.py
+++ lars/preprocessing/radar_preprocessing.py
@@ -9,6 +9,7 @@ import cmweather    # noqa
 def preprocess_radar_data(file_path, output_path,
                           radar_field='corrected_reflectivity',
                           x_bounds=(-150000, 150000), y_bounds=(-150000, 150000),
+                          sweepnum=0,
                           **kwargs):
     """
     Preprocess cf/Radial radar data from a given file path. This module will load the radar data,
@@ -23,6 +24,7 @@ def preprocess_radar_data(file_path, output_path,
         default is 'corrected_reflectivity'.
     x_bounds (tuple): The x-axis bounds for plotting in meters.
     y_bounds (tuple): The y-axis bounds for plotting in meters.
+    sweepnum (int): The sweep to use for the radar image.
 
     **kwargs:
 
@@ -52,21 +54,16 @@ def preprocess_radar_data(file_path, output_path,
         radar = xd.io.open_cfradial1_datatree(file)
         # Example preprocessing step: plot and save the 0.5 degree tilt
         radar = radar.xradar.georeference()
-        if 'sweep_0' in radar:
-            sweep = radar['sweep_0']
+        if f'sweep_{sweepnum}' in radar:
+            sweep = radar[f'sweep_{sweepnum}']
             if sweep["sweep_mode"] == 'ppi' or sweep["sweep_mode"] == 'sector':
-                fig = plt.figure(figsize=(4, 4))
+                fig = plt.figure(figsize=(6, 6))
                 ax = plt.axes()
                 sweep["corrected_reflectivity"].plot(x="x", y="y", 
-                                                     add_colorbar=False, 
                                                      ax=ax, 
                                                      **kwargs)
                 ax.set_xlim(x_bounds)
                 ax.set_ylim(y_bounds)
-                ax.axis('off')
-                ax.set_title('')
-                ax.set_xlabel('')
-                ax.set_ylabel('')
                 fig.tight_layout()
                 file_name = os.path.join(output_path,
                                          os.path.basename(file).replace('.nc', '.png'))
@@ -74,7 +71,7 @@ def preprocess_radar_data(file_path, output_path,
                 label = "UNKNOWN"   # Placeholder for actual label extraction logic
                 fig.savefig(os.path.join(output_path,
                                          os.path.basename(file).replace('.nc', '.png')),
-                            dpi=100)
+                            dpi=150)
                 plt.close(fig)
                 out_df.loc[len(out_df)] = [file_name, time_str, label]
             else:
